36d8a9885d2116a64c57840590b74c0e4a185a9d
KARIYA-JAY <125731797+KARIYA-JAY@users.noreply.github.com>
2024-12-21 14:59:09 +0530

    Create Data_Type.js
    commit

---
+++ Data_Type.js
@@ -0,0 +1,22 @@
+let name = "jay"
+let age = 20 
+let LoggedIn = true 
+let height = null
+let state ; 
+
+console.log(name)
+console.log(typeof name)
+
+console.log(age)
+console.log(typeof age)
+
+console.log(LoggedIn)
+console.log(typeof LoggedIn)
+
+console.log(height)
+console.log(typeof height)
+
+
+console.log(typeof undefined)
+
+console.log(typeof null) // null has object type not a special type like undefine  
