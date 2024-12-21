f34c0d65d112d58eb347d2947e42c3d308aa92ca
KARIYA-JAY <125731797+KARIYA-JAY@users.noreply.github.com>
2024-12-21 12:32:34 +0530

    variable and it use
    commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+const name = "jay" 
+var age = 20 
+let height = 6.2
+city = "Gandhinagar" 
+let status ; // it automatically take undefine 
+
+
+// conat cant change 
+// let and var use for mainly declare var but var is block scope so we can use let 
+console.log(name)
+// consol.table is use for printing one or more variable in tabluer format 
+console.table([name,age,height,city,status])
