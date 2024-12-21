13f36201870ff1f3a9742f8d3eb6b9f23e1fcd3d
KARIYA-JAY <125731797+KARIYA-JAY@users.noreply.github.com>
2024-12-21 15:13:32 +0530

    Create Type_conversion.js

---
+++ Type_conversion.js
@@ -0,0 +1,22 @@
+let age = "33" 
+
+console.log(typeof age)
+
+let stringtonumber = Number (age)
+console.log(typeof stringtonumber)
+console.log(stringtonumber)
+
+// "33" =  33 
+// "33ab" = NAN
+// true = 1 / false = 0
+
+let status = 1
+let str = ""
+let str1 = "xdfcgbhj" // true 
+
+let numbertobolean  = Boolean (status) // true 
+console.log(numbertobolean)
+
+let strtoboolean  = Boolean (str) // false 
+console.log(strtoboolean)
+
