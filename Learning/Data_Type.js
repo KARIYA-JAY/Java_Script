063c0d73c8de321844e64d076afaa0926471ad42
KARIYA-JAY <125731797+KARIYA-JAY@users.noreply.github.com>
2024-12-22 14:05:37 +0530

    Update Data_Type.js

--- Learning/Data_Type.js
+++ Learning/Data_Type.js
@@ -20,3 +20,22 @@ console.log(typeof height)
 console.log(typeof undefined)
 
 console.log(typeof null) // null has object type not a special type like undefine  
+
+
+//------------------------------object-----------------------------------
+
+let user = {
+    name:"jay",
+    id:"123@gmail.com" ,
+    age: 20,
+}
+let useerr = user 
+
+useerr.name = "rohit" 
+useerr.age=21
+
+console.log(useerr.age,useerr.name)
+console.log(user.age,user.name)
+
+// if we change one object data that come both use same refrence so it change main object refrence not single object 
+// stack work same but it work for valuse in primitive if we do same as above it make a copy of first var not use refrence
