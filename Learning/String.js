a3ef0ee6998d186cfdd3a550b7d63ccf3efaf53f
KARIYA-JAY <125731797+KARIYA-JAY@users.noreply.github.com>
2024-12-22 14:30:42 +0530

    Create String.js

---
+++ Learning/String.js
@@ -0,0 +1,17 @@
+let name = new String("  jay")
+console.log(name)
+
+let name1 =  name.trim()
+console.log(name1)
+
+let nameSubstring = name.substring(0,1)
+console.log(nameSubstring)
+
+
+let nameslice = name.slice(1,2)
+console.log(nameslice)
+
+console.log(name.trim())
+
+console.log(name.replace('j','s').trim())
+
