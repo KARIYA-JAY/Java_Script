188a81909022a884828e0caf248ba50a8cf6cc90
KARIYA-JAY <125731797+KARIYA-JAY@users.noreply.github.com>
2024-12-23 12:52:10 +0530

    Create num&math.js

---
+++ Learning/num&math.js
@@ -0,0 +1,22 @@
+const balance = 500
+
+// console.log(balance)
+// console.log(typeof balance)
+// console.log(typeof balance.toString())
+// console.log(balance.toFixed(2))
+// console.log(balance)
+
+
+//-------------------maths--------------
+
+const s = [20, 45];
+const maxValue = Math.max(...s);
+console.log(maxValue); // Output: 45
+console.log(Math.max(45 ,8,9,999))
+console.log(Math.min(s))
+
+
+console.log(Math.random().toFixed(3)) // give a random number between 0 to 1 
+min = 1 
+max = 6
+console.log(Math.floor((Math.random() * max- min )+ 1)+ min ) // it gives random number between 1 to 6
