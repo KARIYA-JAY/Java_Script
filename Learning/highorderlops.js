4f9cbf8b46dd98746440b4dbe1de042f69ca36c0
jay <jkariya524@rku.ac.in>
2025-02-06 19:13:11 +0530

    higher order loops

--- Learning/highorderlops.js
+++ Learning/highorderlops.js
@@ -1,1 +1,36 @@
-console.log("hiiiii its high order loops ")
+const arr = [1,2,3,3,5,9,20]
+
+// for of loop
+
+for (const i of arr) {
+    console.log(i)
+}
+
+const new1 = new Map
+new1.set('name',"jay")
+new1.set('age',20)
+
+for (const [j, values] of new1)
+{
+    console.log(j," - ", values)
+}
+
+//------------------------------------------------
+
+
+
+const obj ={
+    name :"jay",
+    age : 20
+}
+for (const element in obj) {
+    console.log(obj[element])
+}
+
+for (const key in arr) {
+    console.log(arr[key])
+}
+//----------------------------------------------------
+
+// for each 
+
